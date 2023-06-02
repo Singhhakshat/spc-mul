572a2835328c4eb4a888509842debff9458e2ac5
Singhhakshat <50708393+Singhhakshat@users.noreply.github.com>
2023-06-02 15:14:18 +0530

    updated origin for cors

--- server.js
+++ server.js
@@ -90,7 +90,7 @@ const httpServer = require("http").createServer(app);
 const PORT = process.env.PORT || 3000;
 const io = require("socket.io")(httpServer, {
     cors: {
-      origin: "http://127.0.0.1:5500",
+      origin: "https://singhhakshat.github.io",
       methods: ["GET", "POST"]
     }
   });
